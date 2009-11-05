86156877f2d769728861c351f6f5c72fb7a9ee27
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-05 21:06:03 +0900

    added the albumImage module

---
+++ apps/pc_frontend/modules/albumImage/actions/actions.class.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * albumImage actions.
+ *
+ * @package    OpenPNE
+ * @subpackage albumImage
+ * @author     Kousuke Ebihara <ebihara@tejimaya.com>
+ * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
+ */
+class albumImageActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfWebRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
