5eef9b59f70b92dade40f617e716e00ab1955a58
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-05 20:48:36 +0900

    added ability to display album informations in album detail page

--- apps/pc_frontend/modules/album/templates/showSuccess.php
+++ apps/pc_frontend/modules/album/templates/showSuccess.php
@@ -1,33 +1,33 @@
-<?php// use_helper('opAlbum', 'Text') ?>
+<?php use_helper('opAlbum') ?>
 
 <?php decorate_with('layoutB') ?>
 <?php slot('op_sidemenu', get_component('album', 'sidemenu', array('member' => $member))) ?>
 
-<?php /* {{{ albumDetailBox */ ?>
 <div class="dparts albumDetailBox"><div class="parts">
-<div class="partsHeading"><h3><?php echo __('album of %1%', array('%1%' => $member->getName())) ?></h3>
+<div class="partsHeading"><h3><?php echo $album->getTitle(); ?></h3>
 </div>
 
-<dl>
-<dd>
-<div class="title">
-<p class="heading"><?php echo $album->getTitle(); ?></p>
-</div>
-<div class="body">
-<?php $images = $album->getAlbumImagesJoinFile() ?>
-<?php if (count($images)): ?>
-<ul class="photo">
-<?php foreach ($images as $image): ?>
-<li><a href="<?php echo sf_image_path($image->getFile()) ?>" target="_blank"><?php echo image_tag_sf_image($image->getFile(), array('size' => '120x120')) ?></a></li>
-<?php endforeach; ?>
-</ul>
-<?php endif; ?>
-<?php echo op_url_cmd(nl2br($album->getBody())) ?>
-</div>
-</dd>
-<dt><?php echo nl2br(op_format_date($album->getCreatedAt(), 'XDateTimeJaBr')) ?></dt>
-<dd><?php echo link_to('写真を追加', 'album/add/?id='.$album->getId()) ?></dd>
-</dl>
+<table>
+<tr><td rowspan="4">
+<?php echo image_tag_sf_image($album->getFile(), array('size' => '180x180')) ?>
+</td>
+<th><?php echo __('Description') ?></th>
+<td colspan="2"><?php echo $album->getBody() ?></td>
+</tr>
+<tr>
+<th><?php echo __('Publication') ?></th>
+<td colspan="2"><?php echo $album->getPublic_flag() ?></td>
+</tr>
+<tr>
+<th><?php echo __('Created') ?></th>
+<td colspan="2"><?php echo op_format_date($album->getCreatedAt(), 'XDateTimeJaBr') ?></td>
+</tr>
+<tr>
+<td colspan="3"><?php echo link_to('写真を追加', 'album/add/?id='.$album->getId()) ?></td>
+</tr>
+</table>
+
+
 <div class="partsHeading"><h3><?php echo __('photo list') ?></h3>
 </div>
 <?php $images = $album->getAlbumImagesJoinFile() ?>
@@ -44,5 +44,3 @@
 <?php endif; ?>
 
 </div></div>
-<?php /* }}} */ ?>
-
