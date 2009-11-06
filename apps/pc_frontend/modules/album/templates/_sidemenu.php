d16f461ef347960d3021ec6cc30ff5c27458c995
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-07 02:37:50 +0900

    fixed sidemenu that has links to diary page

--- apps/pc_frontend/modules/album/templates/_sidemenu.php
+++ apps/pc_frontend/modules/album/templates/_sidemenu.php
@@ -5,7 +5,7 @@
 <div class="partsHeading"><h3><?php echo __('Recently Posted Albums') ?></h3></div>
 <ul>
 <?php foreach ($recentAlbumList as $_album): ?>
-<li><?php echo link_to(op_album_get_title_and_count($_album), 'diary_show', $_album) ?></li>
+<li><?php echo link_to(op_album_get_title_and_count($_album), 'album_show', $_album) ?></li>
 <?php endforeach; ?>
 </ul>
 </div>
