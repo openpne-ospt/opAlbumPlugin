19b3015c4074323be68d9080afa223259bdb00d4
Nguyen Ngoc Tu <tu3009@gmail.com>
2010-06-16 18:12:34 +0900

    fixed some bugs

--- apps/pc_frontend/modules/albumLike/templates/_list.php
+++ apps/pc_frontend/modules/albumLike/templates/_list.php
@@ -1,27 +1,16 @@
 <?php
-  if (count($members) == 1) 
-  {
-    echo __('%user1% likes it', array(
-    '%user1%' => link_to($members[0]->name, 'member/profile?id='.$members[0]->id)));
+$users = '';
+for ($i=0;$i<count($members);$i++)
+{
+  if (0 < $i) 
+  { 
+    $users = $users.', ';
   }
-  else if (count($members) == 2) 
-  {
-    echo __('%user1% and %user2% like it', array(
-    '%user1%' => link_to($members[0]->name, 'member/profile?id='.$members[0]->id),
-    '%user2%' => link_to($members[1]->name, 'member/profile?id='.$members[1]->id)));
-  }
-  else if (count($members) == 3) 
-  {
-    echo __('%user1%, %user2% and %user3% like it', array(
-    '%user1%' => link_to($members[0]->name, 'member/profile?id='.$members[0]->id),
-    '%user2%' => link_to($members[1]->name, 'member/profile?id='.$members[1]->id),
-    '%user3%' => link_to($members[2]->name, 'member/profile?id='.$members[2]->id)));
-  }
-  else if (count($members) > 3) 
-  {
-    echo __('%user1%, %user2% and %others% like it', array(
-    '%user1%' => link_to($members[0]->name, 'member/profile?id='.$members[0]->id),
-    '%user2%' => link_to($members[1]->name, 'member/profile?id='.$members[1]->id),
-    '%others%' => link_to('others('.(count($members)-2).')', '')));
-  }
+  $users = $users.link_to($members[$i]->name, '@member_profile?id='.$members[$i]->id);
+}
+
+if (0 < count($members))
+{
+  echo __('%users% like it', array('%users%' => $users));
+}
 ?>
