7bda4be72bc03fe765c15a2e5ef73b768a9cb943
Shogo Kawahara <kawahara@bucyou.net>
2011-09-03 16:35:47 +0900

    fixed a template for the album management screen on the pc_backend, because it can not show album's cover image (fixes #2348)

--- apps/pc_backend/modules/album/templates/_album.php
+++ apps/pc_backend/modules/album/templates/_album.php
@@ -3,4 +3,4 @@
 <tr><th><?php echo __('Author') ?></th><td><?php echo $album->Member->name ?></td></tr>
 <tr><th><?php echo __('Created at') ?></th><td><?php echo op_format_date($album->created_at, 'XDateTimeJa') ?></td></tr>
 <tr><th><?php echo __('Body') ?></th><td><?php echo nl2br($album->body) ?></td></tr>
-<tr><th><?php echo __('CoverImage') ?></th><td><?php echo image_tag_sf_image($album->getFile(), array('size' => '120x120')) ?></td></tr>
+<tr><th><?php echo __('CoverImage') ?></th><td><?php echo image_tag_sf_image($album->getCoverImage(), array('size' => '120x120')) ?></td></tr>
